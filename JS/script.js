abc70fb827493f04e4788624ca8209a372864539
Muskan-1813 <muskan987654kumari@gmail.com>
2025-07-23 22:27:11 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,177 @@
+// console.log("Hii Javascript");
+// let a = 10;
+// let b = 7;
+// let sum = a+b;
+// console.log("Sum is:",sum);
+
+// //Templates
+// let pencil=20;
+// let pen=21;
+// console.log(`The price of pen and pencil is ${pencil+pen} rupees`);
+
+// //Arithematic Operators
+// let c=21;
+// let d=3;
+// console.log(c+d);
+// console.log(c-d);
+// console.log(c*d);
+// console.log(c/d);
+// console.log(c%d);
+// console.log(c**d);
+
+//Conditional Statements
+// console.log("Before my print statement");
+// let age=2;
+// if(age>18){
+//     console.log("You can vote");
+//     console.log("You can Drive");
+// }
+
+// if(age<18){
+//     console.log("You can't vote");
+//     console.log("You can't Drive");
+// }
+// console.log("After my print statement");
+
+// //Traffic light symbol
+// let color= "Yellow";
+// if(color=="Red"){
+//     console.log("Stop");
+// }
+// if(color=="Yellow"){
+//     console.log("Ready to stop");
+// }
+// if(color=="Green"){
+//     console.log("Go");
+// }
+
+// //Calculate popcorn prizes
+// let size="XL";
+// if(size==="XL"){
+//     console.log("Price is Rs.250");
+// }
+// else if(size==="LL"){
+//     console.log("Price is Rs.2o0");
+// }
+// else if(size==="M"){
+//     console.log("Price is Rs.100");
+// }
+// else{
+//     console.log("Price is Rs.50");
+// }
+    
+// str1="at";
+// if(str1[0]=="a" && (str1.length)<3){
+//     console.log("Good string");
+// }
+
+// let day=7;
+
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("OOps!");
+
+// }
+
+// alert('something is wrong!');
+// console.error("THis is error message");
+// console.warn("THis is warning message");
+// prompt("enter ur name:");
+
+
+// let fName=prompt("Enter ur first name:");
+// let lName=prompt("Enter ur last name:");
+// console.log(`Your name is ${fName+ " "+lName}`);
+
+//Assignment begins Here!!
+
+// Createanumbervariablenumwithsomevalue.Now,print“good”ifthenumberisdivisibleby10andprint“bad”ifitisnot
+// let num = 11;
+// if(num%10==0){
+//     console.log("Good");
+// }
+// else{
+//     console.log("Bad");
+// }
+
+//Question 2
+// let name1=prompt("Enter ur name");
+// let age=prompt("Enter ur age");
+// alert(`${name1} is ${age} years old`);
+
+//Question 3
+// let quarterValue = 3;
+// switch(quarterValue){
+//     case 1:
+//         console.log("Jan,feb,mar");
+//         break;
+//     case 2:
+//         console.log("apr,may,jun");
+//         break;
+//     case 3:
+//         console.log("July,august,sep");
+//         break;
+//     case 4:
+//         console.log("oct,nov,dec");
+//         break;
+// }
+
+//Queston 4:Golden String
+// let str2="Aer";
+// if(((str2[0]=="A")||(str2[0]=="a"))&&((str2.length)>5)){
+//     console.log("Golden string");
+// }
+// else{
+//     console.log("Not Golden");
+// }
+
+//Questin 5:LARGEST of three numbers
+// let a=1;
+// let b=5;
+// let c=9;
+// if(a>b&&a>c){
+//     console.log("a is largest");
+// }
+
+// else if(b>a&&b>c){
+//     console.log("b is largest");
+// }
+
+// else{
+//     console.log("c is largest")
+// }
+
+//Question 6:Check last digit
+let num1 = 32;
+let num2 = 478532;
+
+if ((num1 % 10) === (num2 % 10)) {
+    console.log("Same last digits");
+} else {
+    console.log("Not same last digits");
+}
+
+
+
+
