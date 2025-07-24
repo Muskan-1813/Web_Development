ad03a22909143c5fe0d6c9cb718d9fb84a13c251
Muskan-1813 <muskan987654kumari@gmail.com>
2025-07-24 09:46:28 +0530

    Add files via upload

---
+++ JS_Strings/script.js
@@ -0,0 +1,17 @@
+//TRIM:Removes only the spaces at beginning and end
+// let msg="   I am Muskan Kumari        ";
+// // console.log(msg.trim());
+// // console.log(msg.toUpperCase());
+// // console.log(msg.toLowerCase());
+// // console.log(msg.indexOf("M"));
+// let newMsg=msg.trim().toUpperCase();
+// console.log(newMsg);
+// console.log(msg.slice(8));
+// console.log(msg.replace("M","N"));
+// console.log(msg.repeat(7));
+
+let msg = "help";
+console.log(msg.trim().toUpperCase());
+
+
+
