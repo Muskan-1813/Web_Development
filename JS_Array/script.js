667210c2fd47fe7bad012d1e501671dbbf5d067a
Muskan-1813 <muskan987654kumari@gmail.com>
2025-07-24 13:33:53 +0530

    Add files via upload

---
+++ JS_Array/script.js
@@ -0,0 +1,71 @@
+// let students =["mk","nk","ns"];
+// console.log(students[0][0]);
+// students[4]="nkl";
+// console.log(students);
+// students.push("asdk");
+// console.log(students);
+// students.pop("asdk");
+// console.log(students);
+// students.unshift("data");
+// console.log(students);
+// students.shift("data");
+// console.log(students);
+
+
+//Practice Qs
+// let start=["january","july","march","august"];
+// start.shift();
+// start.shift();
+// start.unshift("june");
+// start.unshift("july");
+// let final=start;
+// Using splice
+// let start=["january","july","march","august"];
+
+// primary=["red","blue","green"];
+// console.log(primary.indexOf("red"));
+// console.log(primary.indexOf("yellow"));
+// console.log(primary.includes("red"));
+// console.log(primary.includes("purple"));
+// secondary=["purple","violet","brown"];
+// console.log(primary.concat(secondary));
+// console.log(primary.reverse());
+// console.log(primary.slice(-1));
+// console.log(primary.splice(0,1));
+// primary.splice(0,0,"violet");
+// //Syntax of splice:(start,count of delete,elements to be added..)
+
+// //Sort
+// primary.sort();
+
+//TIC TAC TOE
+// console.log([["X",null,"0"],[null,"X",null],["O",null,"X"]]);
+
+
+//Assignment Questions
+
+// arr=[7,9,0,-2];
+// console.log(arr.slice(0,3));
+// console.log(arr.slice(1,4));
+
+// str="hey";
+// if(str.length==0){
+//     console.log("Blank");
+// }
+// else{
+//     console.log("Not blank");
+// }
+
+str1="hiniddey";
+if(str1[0]==str1[0].toLowerCase)
+    {console.log("Lower");
+                 }
+
+else{
+    console.log("no");
+}
+
+
+
+
+
